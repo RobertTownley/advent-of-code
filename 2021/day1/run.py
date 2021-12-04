7ed12bd26c05924dfcf765229ee6ea5c450b41e9
Robert Townley <me@roberttownley.com>
2021-12-04 13:51:25 -0500

    Finish day 1

--- 2021/day1/run.py
+++ 2021/day1/run.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-# filepath = "input.txt"
-filepath = "sample.txt"
+filepath = "input.txt"
+# filepath = "sample.txt"
 
 df = pd.read_csv(filepath, names=["value"])
 
@@ -17,5 +17,19 @@ for value in values:
 print(f"In part 1, the value increased {increase_count} times")
 
 # Part 2: Count the # of times the sliding 3 window increased
+pairings = [[values[0]]]
+sums = []
+for value in values[1:]:
+    previous_pairing = pairings[-1]
+    pairings.append([*previous_pairing, value][-3:])
+    if len(pairings[-1]) == 3:
+        sums.append(sum(pairings[-1]))
+
 current_value = None
 increase_count = 0
+for value in sums:
+    if current_value and value > current_value:
+        increase_count += 1
+    current_value = value
+
+print(f"In part 2, the value increased {increase_count} times")
