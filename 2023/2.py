889c8e8a103abe15c18778f9e8908a35f78b83d4
Robert Townley <me@roberttownley.com>
2023-12-02 11:22:46 -0500

    2023

---
+++ 2023/2.py
@@ -0,0 +1,22 @@
+USE_REAL = False
+PART = 1
+
+sample_input  = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+with open("2.txt") as f:
+    real_input = f.read()
+
+def part_one():
+    print("Part one")
+
+def part_two():
+    print("Part two")
+
+
+if PART == 1:
+    part_one()
+elif PART == 2:
+    part_two()
